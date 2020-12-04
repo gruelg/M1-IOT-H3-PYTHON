33e23d4d06487b64da625f7398e1d52512a92e00
GGUL <guillaume.gruel.prestataire@grassavoye.com>
2020-12-05 00:29:26 +0100

    ajout page projet

--- Mini Projet/main.py
+++ Mini Projet/main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 import os
+from flask import request
+import flask
+import json
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,6 +16,13 @@ def resume():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/projet')
+def projet():
+    projet = json.load(open('projet.json'))
+    #return flask.jsonify(projet)
+    return render_template('projet.html', projets=projet)
+
 
 if __name__ == "__main__":
     #port = int(os.environ.get('PORT', 5000))
