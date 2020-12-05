9cd033eb297e44f80318e7c3f78d74e074002ed6
GGUL <guillaume.gruel.prestataire@grassavoye.com>
2020-12-05 21:37:43 +0100

    ajout docstring

--- Mini Projet/scripts/main.py
+++ Mini Projet/scripts/main.py
@@ -1,24 +1,35 @@
 from flask import Flask, render_template, redirect, url_for
-import os
 from flask import request
-import flask
 import json
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+   """
+    renvoie le template de base quand le route correspond a /
+   """
    return render_template('base.html')
 
 @app.route('/resume')
 def resume():
+    """
+        renvoie le template de base quand le route correspond a /resume
+    """
     return render_template('resume.html')
 
 @app.route('/contact')
 def contact():
+    """
+        renvoie le template de base quand le route correspond a /contact
+    """
     return render_template('contact.html')
 
 @app.route('/contact', methods=['POST'])
 def text_box():
+    """
+        renvoie le template de base quand le route correspond a /contact
+        avec des données d'une requete POST et les stock dans un fichier json
+    """
     response = {}
     response['nom'] = request.form['nom']
     response['prenom'] = request.form['prenom']
@@ -30,13 +41,13 @@ def text_box():
 
 @app.route('/projet')
 def projet():
+    """
+        renvoie le template projet quand le route correspond a /projet
+        et affiche tous les projets d'apres les données enregistrer dans le fichier json
+    """
     projet = json.load(open('projet.json'))
-    #return flask.jsonify(projet)
     return render_template('projet.html', projets=projet)
 
 
 if __name__ == "__main__":
-    #port = int(os.environ.get('PORT', 5000))
-    #host='172.0.0.1'
-    #app.run(host=host, port=port)
     app.run(debug=True)
