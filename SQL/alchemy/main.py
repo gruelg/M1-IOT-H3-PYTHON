d6b9eb2fb7f82b31cd29f75fe2875e7627dac9da
GGUL <guillaume.gruel.prestataire@grassavoye.com>
2021-02-03 12:21:12 +0100

    fin de matinée 03/02

--- SQL/alchemy/main.py
+++ SQL/alchemy/main.py
@@ -1,10 +1,11 @@
-from sqlalchemy import create_engine
-
+from sqlalchemy import *
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import sessionmaker
 config = {
     'host': 'localhost',
     'port': '3306',
     'user': 'root',
-    'password':'my-secret-pw',
+    'password':'hitema',
     'database':'classicmodels'
 }
 db_user = config.get('user')
@@ -16,3 +17,22 @@ db_name = config.get('database')
 connexion_str = f'mysql+pymysql://{db_user}:{db_pwd}@{db_host}:{db_port}/{db_name}'
 engine = create_engine(connexion_str)
 connexion = engine.connect()
+##sql raw ->
+rs = connexion.execute('SELECT * FROM payments')
+
+for row in rs:
+    print(row)
+
+##metadata
+metadata = MetaData()
+metadata.reflect(engine)
+Base = automap_base(metadata=metadata)
+Base.prepare()
+##
+payments= Base.classes.payments
+##session
+Session = sessionmaker(bind = engine)
+session = Session()
+result = session.query(payments).all()
+for row in result:
+    print("customer number : ",row.customerNumber,"checknumber :", row.checkNumber,"paymentdate :", row.paymentDate,"amount :", row.amount)
--- SQL/requestes.sql
+++ SQL/requestes.sql
@@ -26,3 +26,23 @@ select distinct count(productName) from products;
 select customerName, city from customers where salesRepEmployeeNumber is NULL;
 --14 
 select CONCAT(lastName," ", firstName) from employees where jobTitle like 'VP%' or jobTitle like '%Manager%';
+--15
+select orderNumber, SUM(quantityOrdered*priceEach ) AS prix_total from orderdetails having prix_total > 5000;
+--16
+select CONCAT(customers.contactLastName," ", customers.contactLastName) as customer ,CONCAT(employees.lastName," ", employees.firstName) as employees from customers, employees where customers.salesRepEmployeeNumber = employees.employeeNumber;	
+--17
+select SUM(amount ) AS prix_total from payments where customerNumber = ( select customerNumber from customers where customerName = 'Atelier graphique');
+--18
+select sum(amount), paymentDate from payments group by paymentDate;
+--19
+select productName from products where productCode not in ( select productCode from orderdetails);
+--20
+select CONCAT(customers.contactLastName," ", customers.contactLastName) as customer, sum(payments.amount) as payment from customers, payments where customers.customerNumber = payments.customerNumber group by payments.customerNumber;
+--21
+select count(status) from orders where status ='On Hold';
+--22
+SELECT orderNumber FROM orders where DAYOFWEEK(orderDate) = 2;
+--23
+select products.productName from customers, orders, orderdetails, products where customers.customerNumber = orders.customerNumber and orders.orderNumber = orderdetails.orderNumber and orderdetails.productCode = products.productCode and  customers.customerName = 'Herkku Gifts';
+--24
+select * from orderdetails group by orderNumber, productCode limit 5;
